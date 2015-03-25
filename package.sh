8d7c19f890f85d4234152a2f020e59c109811930
Night <night@nightdev.com>
2015-03-25 17:50:48 -0400

    Teak PLS

--- package.sh
+++ package.sh
@@ -5,19 +5,19 @@ npm install
 n v0.12.0
 
 version=$(node -pe "require(process.argv[1]).version" ./package.json)
-dl="http://dl.nwjs.io/v0.11.6/node-webkit-v0.11.6-osx-x64.zip"
+dl="http://dl.nwjs.io/v0.12.0/nwjs-v0.12.0-osx-x64.zip"
 cwd=$(pwd)
 
 cd node_modules/keytar
 nw-gyp clean
-nw-gyp configure --target=0.10.5 --arch=x64
+nw-gyp configure --target=0.12.0 --arch=x64
 nw-gyp build
 cd $cwd
 
 mkdir -p output && cd output
 curl -vvv $dl -o nw.zip
 rm -R Pussh.app
-unzip nw.zip && mv node-*/node-webkit.app Pussh.app && rm -R node-*
+unzip nw.zip && mv nwjs-*/nwjs.app Pussh.app && rm -R nwjs-*
 
 cd $cwd
 zip --exclude=output* -x=.git* -r output/app.nw .
@@ -25,7 +25,12 @@ cd output
 
 cp app.nw Pussh.app/Contents/Resources/app.nw
 cp ../Resources/img/icon.icns Pussh.app/Contents/Resources/nw.icns
-cp ../Resources/pkg/Info.plist Pussh.app/Contents/Info.plist
+/usr/libexec/PlistBuddy -c "Set :CFBundleDisplayName Pussh" Pussh.app/Contents/Info.plist
+/usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier com.nightdev.pussh" Pussh.app/Contents/Info.plist
+/usr/libexec/PlistBuddy -c "Set :CFBundleDocumentTypes:0:CFBundleTypeName Pussh" Pussh.app/Contents/Info.plist
+/usr/libexec/PlistBuddy -c "Set :CFBundleDocumentTypes:0:LSItemContentTypes:0 com.nightdev.pussh.app" Pussh.app/Contents/Info.plist
+/usr/libexec/PlistBuddy -c "Set :UTExportedTypeDeclarations:0:UTTypeDescription Pussh" Pussh.app/Contents/Info.plist
+/usr/libexec/PlistBuddy -c "Set :UTExportedTypeDeclarations:0:UTTypeIdentifier com.nightdev.pussh.app" Pussh.app/Contents/Info.plist
 /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $version" Pussh.app/Contents/Info.plist
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $version" Pussh.app/Contents/Info.plist
 rm app.nw
